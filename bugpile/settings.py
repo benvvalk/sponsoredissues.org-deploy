2147c971448143f0d79b47fd94a8598828604dc7
Ben Vandervalk <ben.vvalk@gmail.com>
2025-06-25 12:28:39 -0400

    index.html: make it the home page [feat]
    Make the landing page mockup I made in `/index.html` the home page for
    the django project.
    
    Claude Code says:
    
    ```
    ● Done! I've moved index.html to bugpile/templates/ and configured it as your Django
      homepage. The changes include:
    
      - Created bugpile/templates/ directory and moved index.html there
      - Created bugpile/views.py with an index view function
      - Updated bugpile/urls.py to route the root URL to the index view
      - Modified bugpile/settings.py to include the templates directory
    ```

--- bugpile/settings.py
+++ bugpile/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'bugpile.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'bugpile' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
