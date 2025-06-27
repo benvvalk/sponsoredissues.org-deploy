918534d79e4e89cee4593a4947bff2e957114edb
Ben Vandervalk <ben.vvalk@gmail.com>
2025-06-27 15:04:59 -0400

    settings.py: generate random `SECRET_KEY` on startup [security]
    Also, allow `SECRET_KEY` to be overridden by the `DJANGO_SECRET_KEY`
    environment variable. This is useful for local development because it
    prevents the website cookies (e.g. user logins) for getting
    invalidated every time I restart the web server.

--- bugpile/settings.py
+++ bugpile/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+from django.core.management.utils import get_random_secret_key
 from pathlib import Path
 import os
 
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n(@ck&-i74g)h--+*ta8@w1jf60e!89g!9$149w#lsxga1xwmk'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.getenv('DJANGO_DEBUG'))
