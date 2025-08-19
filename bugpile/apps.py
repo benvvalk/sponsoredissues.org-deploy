71c91e849929216a122aed5e5aa5c6bcef378572
Ben Vandervalk <ben.vvalk@gmail.com>
2025-08-19 13:29:06 -0400

    apps.py: only call `init_paypal` when running `./manage.py runserver` [bugfix]
    `init_paypal` was being called on every management command (e.g.
    `./manage.py makemigrations`, `./manage.py migrate`), whereas it
    should only be called when starting the server (i.e. `./manage.py
    runserver`).
    
    It's not a big deal, but it was causing unnecessary calls to the
    PayPal API, and cluttering the Django log with multiple "PayPal
    configured for sandbox mode" messages.

--- bugpile/apps.py
+++ bugpile/apps.py
@@ -1,12 +1,15 @@
 from django.apps import AppConfig
 import logging
+import sys
 
 class BugpileConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bugpile'
 
     def ready(self):
-        from django.conf import settings
-        from .paypal import init_paypal
+        # Only initialize PayPal when running the server
+        if 'runserver' in sys.argv:
+            from django.conf import settings
+            from .paypal import init_paypal
 
-        init_paypal(settings.PAYPAL_CLIENT_ID, settings.PAYPAL_CLIENT_SECRET)
+            init_paypal(settings.PAYPAL_CLIENT_ID, settings.PAYPAL_CLIENT_SECRET)
