1970dae7758a0e6e7c3de2093d1f662d6cdc96de
Ben Vandervalk <ben.vvalk@gmail.com>
2025-08-20 13:05:25 -0400

    apps.py: fix redundant calls to `init_paypal` [bugfix]

--- bugpile/apps.py
+++ bugpile/apps.py
@@ -7,9 +7,16 @@ class BugpileConfig(AppConfig):
     name = 'bugpile'
 
     def ready(self):
-        # Only initialize PayPal when running the server
+        # Only call `init_paypal` when starting the Django server
+        # with the `./manage.py runserver` command, not during other
+        # management commands (e.g. `./manage.py migrate`).
         if 'runserver' in sys.argv:
-            from django.conf import settings
-            from .paypal import init_paypal
-
-            init_paypal(settings.PAYPAL_CLIENT_ID, settings.PAYPAL_CLIENT_SECRET)
+            import os
+            # Prevent `init_paypal` from being called twice due to
+            # Django's file-watcher/reloader process. See the
+            # following notes for further explanation:
+            # https://programmersought.com/article/49721374106/
+            if os.environ.get('RUN_MAIN') == 'true':
+                from django.conf import settings
+                from .paypal import init_paypal
+                init_paypal(settings.PAYPAL_CLIENT_ID, settings.PAYPAL_CLIENT_SECRET)
