837c0095bdcecc7efde36c33da51cb88404f426e
Ben Vandervalk <ben.vvalk@gmail.com>
2026-02-05 11:56:51 -0500

    tasks: factor out `task_sync_github_app_installation_lock` [refactor]

--- sponsoredissues/tasks.py
+++ sponsoredissues/tasks.py
@@ -15,17 +15,20 @@ logger = get_task_logger(__name__)
 def debug_task(self):
     logger.info(f'Request: {self.request!r}')
 
+def task_sync_github_app_installation_lock(installation_url: str):
+    return redis_client.lock(
+        name=f'lock:{installation_url}',
+        timeout=300,        # task must complete before this expires
+        blocking_timeout=0  # return immediately if lock not available
+    )
+
 @app.task(bind=True, ignore_result=True)
 def task_sync_github_app_installation_least_recently_updated(self):
     installations = GitHubAppInstallation.objects.all().order_by("updated_at")
     logger.info(f'database contains {installations.count()} app installations')
 
     for installation in installations:
-        lock = redis_client.lock(
-            name=f'lock:{installation.url}',
-            timeout=300,        # task must complete before this expires
-            blocking_timeout=0  # return immediately if lock not available
-        )
+        lock = task_sync_github_app_installation_lock(installation.url)
         if lock.acquire():
             try:
                 github_sync_app_installation(installation.installation_id())
